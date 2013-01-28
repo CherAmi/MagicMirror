29b91a02c13ff67280ffe2255c47a7c3c1f13a5a
Cher Ami <cherami@tormail.net>
2013-01-28 03:58:15 -0500

    Adding files all at once

---
+++ basement.php
@@ -0,0 +1,12 @@
+<?php
+echo "
+<center>
+	<div class='dp50' style='background-color:#bbb;'><h1 style='color:#444'>Onion</h1></div>
+	<div class='dp50' style='background-color:#444;'><h1 style='color:#bbb'>i2p</h1></div>
+</center>
+</div>
+
+</body>
+</html>
+";
+?>
---
+++ image.php
@@ -0,0 +1,55 @@
+<?php
+include("cryptlib.php");
+$img = $_GET['img'];
+$key = $_GET['key'];
+
+function imgerror($errstr) {
+	header("Content-Type: image/png");
+	$im = imagecreate(300, 25);
+	$background_color = imagecolorallocate($im, 0, 0, 0);
+	$text_color = imagecolorallocate($im, 233, 14, 28);
+	imagestring($im, 2, 5, 5,  $errstr, $text_color);
+	imagepng($im);
+	imagedestroy($im);
+	die();
+}
+
+if(isset($img) && isset($key)) {
+	if(!file_exists("img/$img")) { imgerror("Invalid image file"); }
+	$encimg = file_get_contents("img/$img");
+	$crypt = new Crypt_AES(CRYPT_AES_MODE_ECB);
+	$crypt->setKey($key);
+	$decimg = $crypt->decrypt($encimg);
+
+	if($decimg == "") { imgerror("Incorrect password"); }
+
+	$tempfile = md5(rand(0, 0x7FFFFF));
+	file_put_contents("img/$tempfile", $decimg);
+
+	$imgtype = exif_imagetype("img/$tempfile");
+
+	if($imgtype == IMAGETYPE_GIF) {
+		$im = imagecreatefromgif("img/$tempfile");
+		header("Content-Type: image/gif");
+		imagegif($im);
+	} elseif($imgtype == IMAGETYPE_PNG) {
+		$im = imagecreatefrompng("img/$tempfile");
+		header("Content-Type: image/png");
+		imagegif($im);
+	} elseif($imgtype == IMAGETYPE_JPEG) {
+		$im = imagecreatefromjpeg("img/$tempfile");
+		header("Content-Type: image/jpeg");
+		imagegif($im);
+	} else {
+		$im = imagecreatefromstring($decimg);
+		header("Content-Type: image/gif");
+		imagegif($im);
+	}
+
+	unlink("img/$tempfile");
+
+} else {
+	imgerror("Image or key not set");
+}
+
+?>
