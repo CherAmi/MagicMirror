bb47af0497427504afacf11100547d9f18c7954b
Cher Ami <cherami@tormail.net>
2013-01-28 13:10:02 -0500

    Fixed a formatting error. I forgot a ">"

--- basement.php
+++ basement.php
@@ -1,7 +1,7 @@
 <?php
 echo "
 <center>
-	<a href='http://4344457357774542.onion/'<div class='dp50' style='background-color:#bbb;'><h1 style='color:#444'>Onion</h1></div></a>
+	<a href='http://li7qxmk72kp3sgz4.onion/'><div class='dp50' style='background-color:#bbb;'><h1 style='color:#444'>Onion</h1></div></a>
 	<a href='http://j4mosyh4wdjga2pz4jiwxq5td4n4jnnxeru2kciiv3ebjzsnkcda.b32.i2p/'><div class='dp50' style='background-color:#444;'><h1 style='color:#bbb'>b32.i2p</h1></div></a>
 </center>
 </div>
