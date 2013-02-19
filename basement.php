eec460225b3c385fb6777fd2cbb544032d266230
Cher Ami <cherami@tormail.net>
2013-02-19 14:02:53 -0500

    Lost the private keys to magicmirror.i2p, moved to new host

--- basement.php
+++ basement.php
@@ -2,7 +2,7 @@
 echo "
 <center>
 	<a href='http://li7qxmk72kp3sgz4.onion/'><div class='dp50' style='background-color:#bbb;'><h1 style='color:#444'>li7qxmk72kp3sgz4.onion</h1></div></a>
-	<a href='http://magicmirror.i2p/'><div class='dp50' style='background-color:#444;'><h1 style='color:#bbb'>magicmirror.i2p</h1></div></a>
+	<a href='http://img.i2p/'><div class='dp50' style='background-color:#444;'><h1 style='color:#bbb'>img.i2p</h1></div></a>
 </center>
 </div>
 
