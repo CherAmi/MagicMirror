ccdc3b601f2334bab69910ce8f5ca8870b9ee260
Cher Ami <cherami@tormail.net>
2013-03-05 01:58:18 -0500

    Stylization

--- basement.php
+++ basement.php
@@ -2,9 +2,9 @@
 echo "
 <center>
 <a href='http://img.404.mn/'><div class='dp25' style='background-color:#000;'><h2 style='color:#fff'>img.404.mn</h1></div></a>
-<a href='http://li7qxmk72kp3sgz4.onion/'><div class='dp25' style='background-color:#bbb;'><h2 style='color:#444'>li7qxmk72kp3sgz4.onion</h1></div></a>
+<a href='http://li7qxmk72kp3sgz4.onion/'><div class='dp25' style='background-color:#444;'><h2 style='color:#bbb'>li7qxmk72kp3sgz4.onion</h1></div></a>
 <a href='http://4344457357774542.onion/'><div class='dp25' style='background-color:#bbb;'><h2 style='color:#444'>4344457357774542.onion</h1></div></a>
-<a href='http://img.i2p/'><div class='dp25' style='background-color:#444;'><h2 style='color:#bbb'>img.i2p</h1></div></a>
+<a href='http://img.i2p/'><div class='dp25' style='background-color:#fff;'><h2 style='color:#000'>img.i2p</h1></div></a>
 </center>
 </div>
 
