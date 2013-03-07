680e184d927e6e5e8262d9f994c5e18fec5f5780
Cher Ami <cherami@tormail.net>
2013-03-07 14:04:24 -0500

    Cosmetic changes

--- cron.sh
+++ cron.sh
@@ -1,7 +1,7 @@
-# This file is for gzipping and zipping the images so that 
-# prospective mirror owners can download them. I have it on a once-a-day cron
-# job, but you could run it every minute if you really wanted.
-#
-gzip /var/www/magicmirror/img/*
-zip /var/www/magicmirror/vault.zip /var/www/magicmirror/img/*
-gunzip /var/www/magicmirror/img/*
+# This file is a cron job for prospective mirror owners to run. It gzips and
+# zips up the encrypted images so others can download them and help keep the 
+# service up in case a server gets seized or w/e else happens that causes Magic 
+# Mirror to go down.
+gzip /var/www/magicmirror/img/*;
+zip -9 /var/www/magicmirror/vault.zip /var/www/magicmirror/img/*;
+gunzip /var/www/magicmirror/img/*;
--- index.php
+++ index.php
@@ -26,15 +26,7 @@ echo "
 	If you see something that breaks these rules email me a link and I'll remove it.</p>
 </div>
 <div class='dp50body dtext' style='background-color:#999;'>
-	<h2>Stats</h2>
-	<p><em>Currently indexing <strong>$imgcount</strong> images, consuming a total of <strong>$size MB of disk space</strong> and ~<strong>$bandwidth MB of bandwidth</strong>.</em></p>
-	<h2>Misc</h2>
-	<p>Contact email: <a href='mailto:cherami@tormail.net'>cherami@tormail.net</a>. A PGP key is available <a href='pgp.txt'>here</a>.
-	<br />Bitcoin donations welcome! Address is <em>14JGX4sKbejEzRyWgak98ig7nhoa6Jjpyr</em>. Your donations keep this service alive.
-	<br /></p>
-	<h2>Want to run a mirror?</h2>
-	<p>The source is available <a href='https://github.com/CherAmi/MagicMirror'>here</a> and you can download a zip file of all of the images currently hosted here <a href='vault.zip'>here</a>. Email me at cherami@tormail.net and I'll put a link to your mirror here. Please include when your mirror was last updated on the index page if you run one, so people know what URLs will and won't work.</p><br />
-	<center><h2>Upload<br /></h2></center>
+	<h2>Upload<br /></h2>
 	<div class='dp50'>
 		<center>
 			<h2>File</h2>
@@ -51,12 +43,20 @@ echo "
 			<h2>URL</h2>
 			<form action='upload.php' method='post'>
 				<input type='text' name='url' placeholder='Image URL' style='width: 100%'><br />
+
 				<input type='text' name='plen' placeholder='Desired password length (must be >=2)' style='width: 100%'><br />
 				<input type='submit' value='Upload'>
 			</form>
-
 		</center>
-	</div>
+	</div><br />
+	<h2>Stats</h2>
+	<p><em>Currently indexing <strong>$imgcount</strong> images, consuming a total of <strong>$size MB of disk space</strong> and ~<strong>$bandwidth MB of bandwidth</strong>.</em></p>
+	<h2>Misc</h2>
+	<p>Contact email: <a href='mailto:cherami@tormail.net'>cherami@tormail.net</a>. A PGP key is available <a href='pgp.txt'>here</a>.
+	<br />Bitcoin donations welcome! Address is <em>14JGX4sKbejEzRyWgak98ig7nhoa6Jjpyr</em>. Your donations help keep this service alive.
+	<br /></p>
+	<h2>Want to run a mirror?</h2>
+	<p>The source is available <a href='https://github.com/CherAmi/MagicMirror'>here</a> and you can download a zip file of all of the images currently hosted here <a href='vault.zip'>here</a>. Email me at cherami@tormail.net and I'll put a link to your mirror here. Please include when your mirror was last updated on the index page if you run one, so people know what URLs will and won't work.</p><br />
 </div>
 
 ";
