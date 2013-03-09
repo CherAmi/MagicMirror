520cff0ccf7298ba08c7a2312d1f829961a76099
Cher Ami <cherami@tormail.net>
2013-03-08 22:13:38 -0500

    Now with key

--- album.php
+++ album.php
@@ -8,18 +8,21 @@ if(file_exists("a/$album")) {
 	echo "<div class='ltext' style='background-color:#666;'>";
 	$albumpics = scandir("a/$album/");
 	foreach($albumpics as $pic) {
+		$broken = explode("-", $pic);
+		$img = $broken[0];
+		$key = $broken[1];
 		echo "		
-		<img src='image.php?img=$pic' /><br />
+		<img src='image.php?img=$pic&key=$broken' /><br />
 		";
 	}
 	echo "</div>";
 } else {
 	echo "
-<div class='ltext' style='background-color:#666;'>
-<h2>Error</h2>
-<p>No such album.</p>
-</div>
-";
+	<div class='ltext' style='background-color:#666;'>
+	<h2>Error</h2>
+	<p>No such album.</p>
+	</div>
+	";
 }
 
 include("basement.php");
