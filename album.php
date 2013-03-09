28c629b78276574164f6e669b71d2238724e2f7e
Cher Ami <cherami@tormail.net>
2013-03-08 22:05:57 -0500

    Forgot a file

---
+++ album.php
@@ -0,0 +1,26 @@
+<?php
+include("attic.php");
+
+$album = $_GET['album'];
+$album = preg_replace("/\W/", "", $album);
+
+if(file_exists("a/$album")) {
+	echo "<div class='ltext' style='background-color:#666;'>";
+	$albumpics = scandir("a/$album/");
+	foreach($albumpics as $pic) {
+		echo "		
+		<img src='image.php?img=$pic' /><br />
+		";
+	}
+	echo "</div>";
+} else {
+	echo "
+<div class='ltext' style='background-color:#666;'>
+<h2>Error</h2>
+<p>No such album.</p>
+</div>
+";
+}
+
+include("attic.php");
+?>
